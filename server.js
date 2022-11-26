0e4ad41f12e9229ac9a35f710e33116a430b2053
thienashima <thienajosepha@gmail.com>
2022-11-26 16:38:11 +0300

    Finish login and registration

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const mysql = require("mysql");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.post("/api/login", function (req, res, next) {
+  const { userid, password } = req.body;
+  const conn = mysql.createConnection({
+    host: "localhost",
+    database: "talent_management",
+    password: "",
+    user: "root",
+  });
+  conn.connect(function (error) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("connected");
+    conn.query(
+      `SELECT * FROM student WHERE student_id="${userid}" and password="${password}"`,
+      function (err, results) {
+        if (err) {
+          return console.log(err);
+        }
+        // console.log(results)
+        if (results.length > 0) {
+          res.redirect("/dashboard");
+        } else {
+          res.redirect("/page");
+        }
+      }
+    );
+  });
+});
+app.post("/api/register", function (req, res, next) {
+  const { userid, email, password, name } = req.body;
+  const conn = mysql.createConnection({
+    host: "localhost",
+    database: "talent_management",
+    password: "",
+    user: "root",
+  });
+  conn.connect(function (error) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("connected");
+    conn.query(
+      `INSERT INTO student (student_id,email_address , password, student_name)
+        VALUES ("${userid}","${email}", "${password}", "${name}")`,
+      function (err, results) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Student successfully added");
+      }
+    );
+  });
+  res.redirect("/page");
+});
+// app.get("/",function (req, res,next) {
+//     res.send('<h1>Hello Express!</h1>')
+// })
+
+app.get("/dashboard", function (req, res, next) {
+  res.send("<h1>Welcome home!</h1>");
+});
+
+// app.get("/thiena",function (req, res,next) {
+// res.render("thiena")
+// })
+// app.get("/day", function (req, res, next){
+// res.send('<h1>21st November 2022</h1>')
+// })
+app.get("/page", function (req, res, next) {
+  res.render("page");
+});
+app.get("/login", function (req, res, next) {
+  res.render("login");
+});
+app.post("/login", function (req, res, next) {});
+app.get("/register", function (req, res, next) {
+  res.render("register");
+});
+app.post("/register", function (req, res, next) {});
+
+app.use(function (req, res, next) {
+  res.send("<h1>Page Does not exist</h1>");
+});
+
+app.listen(2000);
